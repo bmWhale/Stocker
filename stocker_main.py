d122a26cb8b747114f7ee0543c38b8d9f355d3b7
bmWhale <bmwhale.c@gmail.com>
2022-07-21 13:23:56 +0800

    Update stcker_main.py

--- stocker_main.py
+++ stocker_main.py
@@ -1,20 +1,14 @@
 import pandas as pd
 import yfinance as yf
-
-# 讀入series
-df = yf.Ticker("2330.TW").history(period="max")
-price = df.Close
-print(price)
-
 from stocker import Stocker
 
-tsmc = Stocker(price)
+# 讀入series
+df = yf.Ticker("3034.TW").history(period="max")
+price = df.Close
 
-model, model_data = tsmc.create_prophet_model(days=90)
-
-tsmc.evaluate_prediction()
-
-tsmc.changepoint_prior_analysis(changepoint_priors=[0.001, 0.05, 0.1, 0.2])
-
-tsmc.predict_future(days=100)
+stock = Stocker(price)
+model, model_data = stock.create_prophet_model(days=90)
+stock.evaluate_prediction()
+stock.changepoint_prior_analysis(changepoint_priors=[0.001, 0.05, 0.1, 0.2])
+stock.predict_future(days=100)
 
